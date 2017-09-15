abb0acee3e446ab744f39ef01f2436e8cddcbbef
mhzed <minhongz@gmail.com>
2017-09-15 12:15:06 +0800

    minor syntax change

--- index.js
+++ index.js
@@ -5,34 +5,34 @@ var fs = require("fs");
 var sourceMap = require("source-map");
 var path = require('path');
 
-const lc =  (str) => {
-  c = 0
+const lc = (str) => {
+  let c = 0
   for (let ch of str) {
-    if (ch == "\n") c++;
+    if (ch === "\n") c++;
   }
   return c
 }
 
 // line to line mapping of sourcemap
-const smOneToOne = (srcfile, src)=>{
+const smOneToOne = (srcfile, src) => {
   "use strict";
-  let map = new sourceMap.SourceMapGenerator({file:srcfile})
+  let map = new sourceMap.SourceMapGenerator({file: srcfile})
   let n = lc(src)
-  for (let line =1; line <= n + 1; line++)
+  for (let line = 1; line <= n + 1; line++) {
     map.addMapping({
       source: srcfile,
-      original : {line, column:0},
-      generated : {line, column:0}
+      original: {line, column: 0},
+      generated: {line, column: 0}
     })
+  }
   return map.toJSON()
 }
-
 
 module.exports = function (data, callback) {
   let smfile = data.filename + ".map";
 
   if (fs.existsSync(smfile)) {
-    transformer(data, (err, mod)=>{
+    transformer(data, (err, mod) => { // eslint-disable-line 
       "use strict";
       if (mod) {
         var smap = JSON.parse(fs.readFileSync(smfile).toString());
@@ -46,9 +46,8 @@ module.exports = function (data, callback) {
       }
       callback(null, mod);
     })
-
   } else {
-    transformer(data, (err, mod)=>{
+    transformer(data, (err, mod) => {
       "use strict";
       if (mod && !mod.map) {
         mod.map = smOneToOne(mod.filename, mod.code)
--- rnst.js
+++ rnst.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
-require("child_process").execSync("react-native start server --transformer node_modules/react-native-sm-transformer/index",
+require("child_process").execSync(
+  "react-native start server --transformer node_modules/react-native-sm-transformer/index",
   {
-    stdio:[0,1,2]
-  });
+    stdio: [0, 1, 2]
+  }
+);
